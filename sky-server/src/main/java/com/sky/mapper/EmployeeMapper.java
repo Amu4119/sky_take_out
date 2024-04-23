8d6784f838c7187c546a72df25c628525be7ecec
Amu4119 <2729994119@qq.com>
2024-04-23 10:02:10 +0800

    苍穹外卖_员工条件分页查询

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,5 +1,7 @@
 package com.sky.mapper;
 
+import com.github.pagehelper.Page;
+import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
@@ -24,4 +26,9 @@ public interface EmployeeMapper {
             "VALUES " +
             "(#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser})")
     void insert(Employee employee);
+
+    /**
+     * 员工条件分页查询
+     * */
+    Page<Employee> page(EmployeePageQueryDTO employeePageQueryDTO);
 }
